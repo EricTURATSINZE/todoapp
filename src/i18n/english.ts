0de74ddc7d75808a90ec69fecb6fe023baff0440
EricTURATSINZE <turatsinzeeric43@gmail.com>
2024-10-29 23:49:25 +0200

    added translations

---
+++ src/i18n/english.ts
@@ -0,0 +1,18 @@
+const translationsEn = {
+        "search": "Search",
+        "workspace": "Workspace",
+        "creative": "creative",
+        "websiteDesign": "Website Design",
+        "limitedAccess": "Limited access",
+        "allTasks": "All Tasks",
+        "todo": "To do",
+        "inProgress": "In Progress",
+        "completed": "Completed",
+        "filterAndSort": "Filter & Sort",
+        "newTask": "New Task",
+        "from23April": "From 23 April",
+        "updated12MinAgo": "Updated 12 min ago",
+        "creativeWebsite": "Creative Website",
+}
+
+export default translationsEn;
---
+++ src/i18n/french.ts
@@ -0,0 +1,18 @@
+const translationsFr = {
+    "search": "Recherche",
+    "workspace": "Espace de travail",
+    "creative": "créatif",
+    "websiteDesign": "Conception de site web",
+    "limitedAccess": "Accès limité",
+    "allTasks": "Toutes les tâches",
+    "todo": "À faire",
+    "inProgress": "En cours",
+    "completed": "Terminé",
+    "filterAndSort": "Filtrer et trier",
+    "newTask": "Nouvelle tâche",
+    "from23April": "À partir du 23 avril",
+    "updated12MinAgo": "Mis à jour il y a 12 min",
+    "creativeWebsite": "Site web créatif",
+}
+
+export default translationsFr;
